43f4018a765dfaad14acdca65fe170aa0fe4e465
Sushmita Bajgain <sushmita@gurzu.com>
2020-09-04 09:51:04 +0545

    FEAT: Add rspec test files

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :role do
-    name { "MyString" }
+    name { "admin" }
   end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a name" do
+    role = Role.new(name: nil)
+    expect(role).to_not be_valid
+  end
+
+  it "is valid with valid attributes" do
+    expect{params = FactoryBot.attributes_for(:role).to be_valid}
+  end
+
+  it "validation for one role" do
+    expect{params = FactoryBot.attributes_for(:name).should validate_uniqueness_of(:role_id)}
+  end
 end
