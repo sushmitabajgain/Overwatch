c61217598f6bc7324e5207f2d835baef1f1d77a1
Sushmita Bajgain <sushmita@gurzu.com>
2020-09-14 12:16:43 +0545

    FIX: Remove unnecessary code

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -8,12 +8,6 @@ module Api
         render json: projects, status: :ok
       end
 
-      # def weekly
-      #   last = Project.last.week
-      #   projects= Project.where(week: last)
-      #   render json: projects, status: :ok
-      # end
-
       def getStatusCount
         @count = [
                   Project.where(status: "On Track").count,
@@ -25,34 +19,8 @@ module Api
         render json: @count ,status: :ok
       end
 
-      def worksheet
-        @session ||= GoogleDrive::Session.from_service_account_key("client_secret.json")
-        @spreedsheet ||= @session.spreadsheet_by_title("Project Data")
-        @worksheet ||= @spreedsheet.worksheets.first
-        @projects = (@worksheet.rows - [@worksheet.rows.first] - [@worksheet.rows.second])
-        @week = @worksheet.rows.first
-        if Project.all == [] || @week.second.to_i > Project.last.week
-          @projects.each do |i|
-            sn = i[0]
-            status = i[1]
-            project = i[2]
-            start_date = i[3]
-            end_date = i[4]
-            extended_date = i[5]
-            notes = i[6]
-            no_of_resources = i[7]
-            no_of_milestone = i[8]
-            completed_milestone = i[9]
-            missed_milestone = i[10]
-            week = @week.second.to_i
-            Project.create(sn: sn, status: status, project: project, start_date: start_date, end_date: end_date, extended_date: extended_date, notes: notes, week: week, no_of_resources: no_of_resources, no_of_milestone: no_of_milestone, completed_milestone: completed_milestone, missed_milestone: missed_milestone )
-          end
-        end
-        projectData = Project.all
-        render json: projectData, status: :ok
-      end
-
       private
+      
       def project_params
         params.permit(:sn, :status, :project, :start_date, :end_date, :extended_date, :notes, :week,
                        :no_of_resources, :no_of_milestone, :completed_milestone, :missed_milestone)
