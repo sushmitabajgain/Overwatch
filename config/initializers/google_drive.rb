b0355cb9f11827651c575aaf1fe3929dfef4ddfe
Sushmita Bajgain <sushmita@gurzu.com>
2020-10-02 17:32:37 +0545

    FIX: Fix environment file

--- config/initializers/google_drive.rb
+++ config/initializers/google_drive.rb
@@ -1,5 +1,14 @@
 # Create Google Credentials File
+file = {
+  'client_id' => ENV['GOOGLE_CLIENT_ID'],
+  'client_secret' => ENV['GOOGLE_CLIENT_SECRET'],
+  'scope' => [
+    'https://www.googleapis.com/auth/drive',
+    'https://spreadsheets.google.com/feeds/'
+  ],
+  'refresh_token' => ENV['GOOGLE_REFRESH_TOKEN']
+}
 
-File.open('google_credentials.json', 'w') { |w| w.write JSON.parse(ENV['GOOGLE_CREDENTIALS']) }
+File.open('google_credentials.json', 'w') { |w| w.write file.to_json }
 content = File.read('google_credentials.json').gsub('=>', ':')
 File.open('google_credentials.json', 'w') { |w| w.write content }
