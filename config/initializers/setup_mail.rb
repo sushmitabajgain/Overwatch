aa019f1c844f4eacda46f84f15ccdc1bcb2af25f
Sushmita Bajgain <sushmita@gurzu.com>
2020-10-02 16:48:25 +0545

    FIX: FIX sendgrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   user_name:     ENV['SENDGRID_USERNAME'],
   password:      ENV['SENDGRID_PASSWORD'],
-  domain:        'gmail.com',
+  domain:        ENV['SENDGRID_DOMAIN'],
   address:       'smtp.sendgrid.net',
   port:          '587',
   authentication: :plain,
