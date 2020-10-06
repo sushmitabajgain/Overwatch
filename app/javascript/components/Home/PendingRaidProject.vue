97d8230ed49a637c258986315513af07ab532066
Sushmita Bajgain <sushmita@gurzu.com>
2020-10-06 10:43:39 +0545

    FEAT: Add pending raid design

--- app/javascript/components/Home/PendingRaidProject.vue
+++ app/javascript/components/Home/PendingRaidProject.vue
@@ -1,35 +1,45 @@
 <template>
-  <v-container class="blue lighten-5 fill-height" fluid>
-    <v-input style="display:none"> {{currentWeek}}</v-input>
-      <v-row align="center" justify=center>
-        <v-col cols="6" sm="4" md="4">
-          <v-card>
-            <v-window class="">
-                <v-row>
-                  <v-col cols="12">
-                    <v-row align="center" justify=center>
-                      <v-card-title > List Of Pending Raid Project</v-card-title>
-                    </v-row>
-                    <v-card-text>
-                      <v-col class="text--accent-3">
-                        <v-row v-for="project in pending_raid" :key="project">
-                          <router-link :to="{ name: 'Project', params: { id: project.id } }">
-                            <p> {{ project }} </p>
-                          </router-link>
-                        </v-row>
-                      </v-col>
-                    </v-card-text>
-                  </v-col>
-                </v-row>
-            </v-window>
-          </v-card>
-        </v-col>
-      </v-row>
-  </v-container>
+  <div>
+    <NavHeader />
+    <Dashboard />
+    <v-container class="blue lighten-5 fill-height" fluid>
+      <v-input style="display:none"> {{currentWeek}}</v-input>
+        <v-row align="center" justify=center>
+          <v-col cols="6" sm="4" md="4">
+            <v-card>
+              <v-window class="">
+                  <v-row>
+                    <v-col cols="12">
+                      <v-row align="center" justify=center>
+                        <v-card-title > List Of Pending Raid Project</v-card-title>
+                      </v-row>
+                      <v-card-text>
+                        <v-col class="text--accent-3">
+                          <v-row v-for="project in pending_raid" :key="project">
+                            <router-link :to="{ name: 'Project', params: { id: project.id } }">
+                              <p> {{ project }} </p>
+                            </router-link>
+                          </v-row>
+                        </v-col>
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+    </v-container>
+  </div>
 </template>
 <script>
 import eventService from '../../eventService'
+import NavHeader from '../Header/NavHeader'
+import Dashboard from '../Dashboard'
 export default {
+  components:{
+    NavHeader,
+    Dashboard
+  },
   data(){
     return{
       pending_raid: [],
@@ -66,3 +76,12 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.fill-height{
+  height: 100vh;
+}
+p{
+  padding: 2px;
+}
+</style>
